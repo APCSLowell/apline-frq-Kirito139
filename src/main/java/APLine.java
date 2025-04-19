fa562b91d7d5c08e6df73982dc971cc987f6490c
Lennier Mulder <99382674+Kirito139@users.noreply.github.com>
2025-04-19 12:05:29 -0700

    First Attempt

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,18 @@
-public class APLine
-{
-  /* your code here */
-  
+public class APLine {
+    /* your code here */
+    int foo, bar, baz;
+
+    public APLine(a, b, c) {
+        foo = a;
+        bar = b;
+        baz = c;
+    }
+
+    public double getSlope() {
+        return -a / b;
+    }
+
+    public boolean isOnLine(x, y) {
+        return a*x + b*y + c = 0;
+    }
 }
